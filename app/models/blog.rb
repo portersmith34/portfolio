cbce5e5f940ee05cd1ee0005e72a3ca31cd80827
Jordan Hudgens <jordan@devcamp.com>
2017-06-26 15:30:10 +0000

    integrated database relationship between topic and blogpost

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -3,5 +3,7 @@ class Blog < ApplicationRecord
   extend FriendlyId
   friendly_id :title, use: :slugged
   
-  validate_presence_of :title, :body
+  validates_presence_of :title, :body
+  
+  belongs_to :topic
 end
